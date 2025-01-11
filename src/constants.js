c7fbf7be79dcbdef6dc166a90937a714b16d8324
Henrique Cabral <hocabral37@gmail.com>
2025-01-11 18:38:28 -0300

    first

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `I'm a Cybersecurity Engineer at Aikido Security, blending my extensive software development background with my passion for enforcing security in almost every programming language.
+  My PC is my cybersecurity workstation.
+  It's where I write code to fortify systems and explore intricate cybersecurity challenges!`,
+  certs: `I hold the CompTIA CySA+ (Cybersecurity Analyst) certification, a testament to my expertise in identifying, analyzing, and mitigating cybersecurity threats. This certification reflects my commitment to staying ahead in the ever-evolving field of cybersecurity and reinforces my skills in proactive defense, risk management, and incident response. It's a vital part of my journey to build secure systems and share that knowledge with others.`,
+  "cs-degree": `I earned my degree in Cybersecurity from PUC Minas, where I developed a strong foundation in protecting digital systems and mitigating risks. My studies covered a wide range of topics, including secure system design, cryptography, network security, and incident response, equipping me with the skills to tackle real-world challenges. This academic background, combined with my professional experience, fuels my passion for creating robust and secure solutions.`,
+  "sofa-table": `My sofa is more than just a piece of furniture, it's the heart of my home. It's where my wife and I spend quality time together.
+  Our three cats also consider the sofa their personal kingdom.
+  This cozy corner of my home is a space filled with love, laughter, and a good dose of feline mischief.`,
+  tv: `For me, video games are more than just entertainment, they're an art form, a way to unwind, and a source of inspiration. Nintendo games, in particular, hold a special place in my heart, from the adventure-packed worlds of The Legend of Zelda to the joyous chaos of Mario Kart. Each session feels like a puzzle to solve, a skill to master, or simply a chance to relive childhood wonder.`,
+  bed: `My bed is my sanctuary, a place to unwind, recharge, and fuel up for the adventures that await, both in the digital world and real life. After a long day of coding, gaming, or tackling cybersecurity challenges, there's nothing like sinking into the comfort of soft sheets and letting my mind drift.`,
+  resume: `For a deeper dive into my professional journey, skillset, and accomplishments, feel free to explore my <a href="https://www.linkedin.com/in/cabralsecurity/" target="_blank">LinkedIn Profile</a>.
+  There, you'll find detailed insights into my experience as a Cybersecurity Engineer, my strong foundation in software development, and my dedication to building secure and innovative solutions. Let's connect and share ideas!`,
+  projects: `While much of my work is behind the scenes in private repositories, my <a href="https://github.com/HenriqueOCabral" target="_blank">Github</a> reflects my commitment to secure and innovative development. It's where I collaborate on impactful projects, solve complex challenges, and continuously refine my skills.`,
+  library: `My library is a curated treasure trove of knowledge and inspiration. It's packed with O'Reilly books, industry guides and an ever-growing collection of internet gems. Here's where I learn about the latest security trends, and brush up on software development practices. Each book and article fuels my curiosity and keeps my skills sharp.`,
+  sanctum: `What a beautiful garden to meditate and enjoy the silence.`,
+  exit: `If you want out, just close this tab.`,
+};
